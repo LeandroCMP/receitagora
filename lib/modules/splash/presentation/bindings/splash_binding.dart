5670aa28e451cd56a62e296a953b771b44c75fd7
LeandroCMP <48723264+LeandroCMP@users.noreply.github.com>
2025-10-07 14:07:47 -0300

    Simplify splash delay before login

--- lib/modules/splash/presentation/bindings/splash_binding.dart
+++ lib/modules/splash/presentation/bindings/splash_binding.dart
@@ -1,13 +1,10 @@
 import 'package:get/get.dart';
 
-import '../../../../core/services/session_service.dart';
 import '../controllers/splash_controller.dart';
 
 class SplashBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SplashController>(
-      () => SplashController(sessionService: Get.find<SessionService>()),
-    );
+    Get.lazyPut<SplashController>(SplashController.new);
   }
 }
--- lib/modules/splash/presentation/controllers/splash_controller.dart
+++ lib/modules/splash/presentation/controllers/splash_controller.dart
@@ -3,12 +3,7 @@ import 'dart:async';
 import 'package:get/get.dart';
 
 import '../../../../app/routes/app_routes.dart';
-import '../../../../core/services/session_service.dart';
-
 class SplashController extends GetxController {
-  SplashController({required this.sessionService});
-
-  final SessionService sessionService;
 
   @override
   void onReady() {
@@ -17,17 +12,6 @@ class SplashController extends GetxController {
   }
 
   Future<void> _navigate() async {
-    try {
-      await sessionService.ensureInitialized().timeout(
-            const Duration(seconds: 5),
-          );
-    } catch (error, stackTrace) {
-      Get.log(
-        'Failed to hydrate session before leaving splash: $error\n$stackTrace',
-        isError: true,
-      );
-    }
-
     await Future.delayed(const Duration(seconds: 3));
 
     Get.offAllNamed(AppRoutes.login);
