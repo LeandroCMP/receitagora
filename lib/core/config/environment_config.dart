da8a75ce1a91bc2a27995e24f8c0e403e605a703
LeandroCMP <48723264+LeandroCMP@users.noreply.github.com>
2025-10-08 20:19:10 -0300

    .

--- lib/core/config/environment_config.dart
+++ lib/core/config/environment_config.dart
@@ -12,9 +12,10 @@ class EnvironmentConfig {
   final String model;
 
   factory EnvironmentConfig.fromEnv() {
-    final apiKey = (dotenv.maybeGet('OPENAI_API_KEY') ?? '').trim();
+    final apiKey = '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'; //(dotenv.maybeGet('OPENAI_API_KEY') ?? '').trim();
     final baseUrl =
-        (dotenv.maybeGet('OPENAI_BASE_URL') ?? 'https://api.openai.com/v1').trim();
+        (dotenv.maybeGet('OPENAI_BASE_URL') ?? 'https://api.openai.com/v1')
+            .trim();
     final model = (dotenv.maybeGet('OPENAI_MODEL') ?? 'gpt-4o-mini').trim();
 
     return EnvironmentConfig(
