080272160d67abb45e272f3cc518d717228e5c63
LeandroCMP <48723264+LeandroCMP@users.noreply.github.com>
2025-10-09 00:35:09 -0300

    fix: instantiate splash controller

--- lib/modules/splash/presentation/bindings/splash_binding.dart
+++ lib/modules/splash/presentation/bindings/splash_binding.dart
@@ -7,8 +7,8 @@ import '../controllers/splash_controller.dart';
 class SplashBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SplashController>(
-      () => SplashController(
+    Get.put<SplashController>(
+      SplashController(
         sessionService: Get.find<SessionService>(),
         firebaseAuth: Get.find<FirebaseAuth>(),
       ),
