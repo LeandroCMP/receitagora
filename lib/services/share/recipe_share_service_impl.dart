8d1b855e96b35b24f9fac64fb9906b3859c2a4ff
LeandroCMP <48723264+LeandroCMP@users.noreply.github.com>
2025-10-15 00:49:18 -0300

    fix: restaurar compatibilidade do compartilhamento

--- lib/services/share/recipe_share_service_impl.dart
+++ lib/services/share/recipe_share_service_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
+import 'package:cross_file/cross_file.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:share_plus/share_plus.dart';
